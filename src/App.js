a53494835f646f12111db5781e887c56f5462cdc
Neel <khamar.neel8@gmail.com>
2021-09-08 16:29:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { PaytmButton } from './paytm-button/paytmButton';
+
+function App() {
+  return (
+    <div>
+      <PaytmButton />
+    </div>
+  );
+}
+
+export default App;
